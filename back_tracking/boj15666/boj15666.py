deab29056ea7c0d7dead5f0cab763c641c284800
giljung456 <uhwan0723@gmail.com>
2021-11-08 12:52:12 +0900

    boj15666

---
+++ back_tracking/boj15666/boj15666.py
@@ -0,0 +1,29 @@
+import sys;
+
+def parseInput():
+  n, m = map(int, sys.stdin.readline().split());
+  arr = list(map(int, sys.stdin.readline().split()));
+  return n, m, sorted(arr);
+
+def backtracking(*args):
+  depth, cur, n, m, arr, result, sequence = args;
+  if (depth == m):
+    result.append(sequence[:]);
+    return;
+  pre = 0;
+  for nxt in enumerate(arr):
+    if (nxt == pre): continue;
+    if (cur > nxt): continue;
+    pre = nxt;
+    sequence.append(nxt);
+    backtracking(depth + 1, nxt, n, m, arr, result, sequence);
+    sequence.pop();
+
+def solution(*args):
+  n, m, arr = args;
+  result, sequence = [], [];
+  backtracking(0, 0, n, m, arr, result, sequence);
+  return result;
+
+result = solution(*parseInput());
+[print(*sequence) for sequence in result];
